5eabd58a940431865db196dae152785c9bfcdf1f
Pujithavege <109502018+Pujithavege@users.noreply.github.com>
2024-02-08 18:52:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var caluclator =require('./localmodule');
+var x=50,y=10;
+console.log("Addition of 50 and 10 :"+caluclator.add(x,y));
+console.log("Subtraction of 50 and 10 :"+caluclator.sub(x,y));
+console.log("Multiplication of 50 and 10 :"+caluclator.mul(x,y));
+console.log("Division of 50 and 10 :"+caluclator.div(x,y));
